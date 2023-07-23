032c0fb35d42a506977c3978099d1421b04b7654
Aditi4296 <aditisingh4296@rediffmail.com>
2023-07-24 01:26:44 +0530

    login page

--- community-page/src/App.js
+++ community-page/src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import { useSelector } from "react-redux";
+import {selectUser } from "./features/userSlice";
 import './App.css';
 import Feed from './Feed';
 import Header from './Header';
 import SideBar from './SideBar';
 import Widgets from './Widgets';
+import Login from "./Login";
 
 function App() {
+
+  const user = useSelector(selectUser)
   return (
     <div className="app">
 
       <Header />
 
+      {!user?(
+        <Login />
+      ):(
       <div className='app__body'>
         <SideBar />
         <Feed />
         <Widgets />
       </div>
+  )}
     </div>
   );
 }
